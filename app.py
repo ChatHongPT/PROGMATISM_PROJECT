77dd8ebbadc83ea5ab8a13e7fa915d3c95870522
김미르 <samkmr0220@gmail.com>
2024-07-03 16:16:07 +0900

    style: CSS 추가

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import json
-import os
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
 # 피드백 데이터를 저장할 리스트
 feedback_data = []
