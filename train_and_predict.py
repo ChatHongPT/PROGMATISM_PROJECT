fca51e5c932bbdadb41f3228e5f45cd73d52b213
김미르 <samkmr0220@gmail.com>
2024-07-29 11:41:12 +0900

    feat: 모델 수정

--- train_and_predict.py
+++ train_and_predict.py
@@ -32,7 +32,8 @@ def predict_word(file_path):
     features = extract_features(file_path)
     features = features.reshape(1, -1)
     prediction = model.predict(features)
-    return prediction[0]
+    prediction_probabilities = model.predict_proba(features)
+    return prediction[0], prediction_probabilities
 
 # test_data 폴더에서 테스트 파일 예측
 test_data_folder = 'test_data'
@@ -40,5 +41,7 @@ test_files = ['hello_test.wav', 'thank_you_test.wav', 'no_test.wav', 'yes_test.w
 
 for test_file in test_files:
     file_path = os.path.join(test_data_folder, test_file)
-    predicted_word = predict_word(file_path)
+    predicted_word, probabilities = predict_word(file_path)
     print(f'파일: "{test_file}"의 예측된 단어: {predicted_word}')
+    for word, prob in zip(model.classes_, probabilities[0]):
+        print(f'    단어: {word}, 확률: {prob * 100:.2f}%')
